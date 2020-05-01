747d819947e0f2d1eed6b02d0b108299f67ff88b
talasher24 <talasher24@gmail.com>
2020-05-01 04:44:48 +0300

    Version 0.0.52
    Done:
    
    * Define configUSE_NEWLIB_REENTRANT from cubeMX
    * Disable heap_4.c generation from cubeMX

--- NUCLEO-F411RE/Inc/FreeRTOSConfig.h
+++ NUCLEO-F411RE/Inc/FreeRTOSConfig.h
@@ -80,6 +80,9 @@
 #define configUSE_CO_ROUTINES                    0
 #define configMAX_CO_ROUTINE_PRIORITIES          ( 2 )
 
+/* The following flag must be enabled only when using newlib */
+#define configUSE_NEWLIB_REENTRANT          1
+
 /* Set the following definitions to 1 to include the API function, or zero
 to exclude the API function. */
 #define INCLUDE_vTaskPrioritySet             1
@@ -134,7 +137,6 @@ standard names. */
 
 /* USER CODE BEGIN Defines */   	      
 /* Section where parameter definitions can be added (for instance, to override default ones in FreeRTOS.h) */
-#define configUSE_NEWLIB_REENTRANT 1
 /* USER CODE END Defines */ 
 
 #endif /* FREERTOS_CONFIG_H */

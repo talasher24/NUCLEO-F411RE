088579f2b0e71ec7351d536f8a67b79b2fbacadf
talasher24 <talasher24@gmail.com>
2020-05-10 14:16:57 +0300

    Version 0.0.65
    COM_setTxBusyFlagOff -> static

--- NUCLEO-F411RE/Inc/types.h
+++ NUCLEO-F411RE/Inc/types.h
@@ -19,7 +19,7 @@
  * Module Preprocessor Constants
  *******************************************************************************/
 
-#define VERSION 			"0.0.64\n"
+#define VERSION 			"0.0.65\n"
 #define HELLO_WORLD 		"Hello World\n"
 #define TICK 				"tick\n"
 #define PING 				"ping\n"
